7ce87f7e4a2fb53df0ba3caff0e9958f446a2416
Roman Ortega Mondaca <romtega@gmail.com>
2017-06-06 23:41:58 -0500

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
 
   def index
@@ -30,7 +31,6 @@ class PortfoliosController < ApplicationController
 end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -41,14 +41,12 @@ end
   end
 
   def show 
-    @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-    # Destroy/delete the record
     @portfolio_item.destroy
+
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.'}
@@ -63,5 +61,9 @@ end
                                       :body,
                                       technologies_attributes: [:name]
                                       )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,7 @@ module ApplicationHelper
    if session[:source]
      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
      content_tag(:p, greeting, class: "source-greeting")
+     
    end
  end
 end
