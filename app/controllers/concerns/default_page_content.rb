38aeb86b69caeee28b902f333f6da356076136e9
Roman Ortega Mondaca <romtega@gmail.com>
2017-06-09 21:41:22 -0500

    Updated styles and added map for contact page

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -4,10 +4,10 @@ module DefaultPageContent
    included do
     before_action :set_page_defaults
   end
-  @seo_keywords = "Roman Ortega Portfolio"
+  @seo_keywords = "Roman Ortega Mondaca Portfolio"
 
   def set_page_defaults
     @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Roman Ortega Portfolio"
+    @seo_keywords = "Roman Ortega Mondaca Portfolio"
   end
 end
